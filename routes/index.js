650fe06e17c5fb53344e96bbb819738e5b991317
hossain24 <haj.ctg@gmail.com>
2022-02-07 19:45:48 +0000

    index route update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send([{ name: "Hassan Jamil", age: 30 },
+  { name: "Hassan Jamil", age: 30 },
+  { name: "Hassan Jamil", age: 30 }
+  ]);
 });
 
 module.exports = router;
